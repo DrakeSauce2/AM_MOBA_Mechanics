cf21e6e4e11b48818bcd286697123b6e9447a54f
Allen Morin <allenmorin38@gmail.com>
2023-10-31 13:14:04 -0500

    Integrated Movement and Stats

---
+++ Assets/_Allen/Scripts/Movement/Character/MovementComponent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MovementComponent
+{
+    public Character Owner { get; private set; }
+    CharacterController controller;
+
+    public MovementComponent(Character owner)
+    {
+        Owner = owner;
+
+        controller = Owner.gameObject.AddComponent<CharacterController>();
+    }
+
+    // To execute move caller must own a stats object and must contain a MOVEMENT_SPEED stat.
+    public void Move(Vector3 movementDirection) 
+    {
+        controller.Move(movementDirection * Owner.GetStats().TryGetStatValue(Stat.MOVEMENT_SPEED) * Time.fixedDeltaTime);
+    }
+}
